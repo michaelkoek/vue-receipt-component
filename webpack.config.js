310d9bc036af251b902e7129d634d8fc7424a078
Michael Koek <michaelkoek@gmail.com>
2017-12-03 06:18:20 +0100

    new npm release way. clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,60 @@
-const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
+const path = require('path');
 
-module.exports = merge(require('./webpack.base'), {
-    context: __dirname,
-
-    entry: './src/receipt/index.js',
-
+const commonConfig = {
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        library: 'vue-receipt-component',
-        libraryTarget: 'umd',
+        path: path.resolve(__dirname + '/dist/'),
+        filename: 'vue-receipt-component.js'
     },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: __dirname,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!less!css'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin( {
+            minimize : true,
+            sourceMap : false,
+            mangle: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
 
-    externals: {
-        vue: 'vue',
-    },
-});
+module.exports = [
+    merge(config, {
+        entry: path.resolve(__dirname + '/src/receipt/plugin.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'vue-receipt-component.min.js',
+            libraryTarget: 'window',
+            library: 'VueReceiptComponent'
+        }
+    }),
+    merge(config, {
+        entry: path.resolve(__dirname + '/src/receipt/index.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'vue-receipt-component.js',
+            libraryTarget: 'umd',
+            library: 'vue-receipt-component',
+            umdNamedDefine: true
+        }
+    })
+];
